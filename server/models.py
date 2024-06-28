1dfb3d3f749e9fcc50e21cd6e9c62a8fc5d3c88a
RumiYo <yokoyamarumi@gmail.com>
2024-06-28 13:15:03 -0400

    Complete lab

--- server/models.py
+++ server/models.py
@@ -6,12 +6,29 @@ class Author(db.Model):
     __tablename__ = 'authors'
     
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if len(name) == 0: 
+            raise ValueError("Name cannot be null") 
+
+        names = [author.name for author in Author.query.all() ]
+        if name in names: 
+            raise ValueError("Name must be unique")
+        return name
+
+    @validates('phone_number')
+    def validation_phone_number(self, key, number):
+        if not number.isdigit():
+            raise ValueError("Phone number must include only number")
+        if len(number) != 10:
+            raise ValueError("Phone number must be 10 digits")
+        return number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +44,32 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators 
+    @validates('title')
+    def validate_title(self, key, title):
+        if len(title) == 0:
+            raise ValueError('Title must be non empty string') 
+        return title
+
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Content must be equal to or longer than 250charactors')
+        return content
+
+    @validates('category')
+    def validate_category(self, key, category):
+        categories = ["Won't Believe", "Secret", "Top", "Guess", "Non-Fiction"]
+        if category not in categories:
+            raise ValueError('Category does not exist')
+        return category
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Summary must be shorter than 250')
+        return summary
+
 
 
     def __repr__(self):
